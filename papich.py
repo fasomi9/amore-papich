f57f4d9ea3e99ca414401437757ea304907f4de8
fasomi9 <62301244+fasomi9@users.noreply.github.com>
2023-04-08 20:21:36 +0500

    Create papich.py

---
+++ papich.py
@@ -0,0 +1,485 @@
+# ---------------------------------------------------------------------------------
+# /\_/\ 🌐 This module was loaded through https://t.me/hikkamods_bot
+# ( o.o ) 🔐 Licensed under the GNU AGPLv3.
+# > ^ < ⚠️ Owner of heta.hikariatama.ru doesn't take any responsibilities or intellectual property rights regarding this script
+# ---------------------------------------------------------------------------------
+# Name: animevoices
+# Author: AmoreForever
+# Commands:
+# .rofl | .pshl | .luch | .author | .mne
+# .hamne | .aaa | .cho | .mank | .norofl
+# .skinte | .vibor | .yo | .rofll | .zaderjka
+# .stupid | .welaw | .gde | .stream | .budi
+# .genius | .kringe | .dota | .debil | .papich
+# .crazy | .vecher | .gimn | .von | .kogo
+# .mujik | .tvarei | .aphci | .jivu
+# ---------------------------------------------------------------------------------
+
+# ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠟⠛⠛⠛⠛⠛
+# ⣶⣦⣤⣤⣤⣤⣤⣤⣬⣭⣭⣍⣉⡙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣋⣩⣭⣥⣤⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶
+# ⣆⠀⠀⠀⢡⠁⠀⡀⠀⢸⠟⠻⣯⠙⠛⠷⣶⣬⡙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢉⣥⣶⡟⠻⣙⡉⠀⢰⡆⠀⠀⣡⠀⣧⠀⠀⠀⢨
+# ⠻⣦⠀⠀⠈⣇⣀⣧⣴⣿⣶⣶⣿⣷⠀⢀⡇⠉⠻⢶⣌⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣡⡶⠟⠉⠀⢣⠀⣿⠷⠀⠀⠀⠀⣿⡷⢀⠇⠀⠀⢠⣿
+# ⣦⡈⢧⡀⠀⠘⢮⡙⠛⠉⠀⠄⠙⢿⣀⠞⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠈⠳⣄⠉⠓⠒⠚⠋⢀⡠⠋⠀⢀⣴⣏⣿
+# ⣿⣿⣿⣛⣦⣀⠀⠙⠓⠦⠤⣤⠔⠛⠁⠀⠀⠀⠀⠀⢀⣀⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣤⣤⣤⣤⣀⣀⣀⣀⢙⢓⣒⡒⠚⠋⢠⣤⢶⣟⣽⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣷⣦⠀⠀⣴⣿⣷⣶⣶⣶⣾⡖⢰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⣿⠀⢀⣿⣿⣿⣿⣿⣿⣿⠃⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⡏⠀⢸⣿⣿⣿⣿⣿⣿⣿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⣿⣿⣿⣿⣿⣿⣿⣷⠀⢸⣿⣿⣿⣿⣿⣿⣿⠀⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# https://t.me/amorescam
+# scope: hikka_only
+# scope: hikka_min 1.5.0
+# meta developer: @amoremods
+
+from .. import loader
+
+@loader.tds
+class papichVoicesMod(loader.Module):
+ """🎤 it's papich"""
+
+ strings = {"name": "PapichVoices"}
+
+ async def roflcmd(self, message):
+    """ рофл """
+
+ reply = await message.get_reply_message()
+ await message.delete( )
+ await message.client.send_file( 
+ message.to_id, 
+    "https://t.me/papichvoice/494",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def pshlcmd(self, message):
+    """пшл нах"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/493",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def luchcmd (self, message):
+    """красава лучший"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/492",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def authorcmd (self, message):
+    """автор ты ебобо"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/491",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def mnecmd (self, message):
+    """ЭТО МНЕ!"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/490",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def hamnecmd (self, message):
+    """АХХАХАХ это мне"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/489",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def aaacmd (self, message):
+    """ААААААААААА"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/487",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def chocmd (self, message):
+    """че с мордой"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/486",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def mankcmd (self, message):
+     """обезьяна"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/485",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def noroflcmd (self, message):
+    """ХАХАХАА не смешно"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/484",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def skintecmd (self, message):
+    """скиньте на лечение"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/483",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def viborcmd (self, message):
+    """выбор ты где"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/481",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def yocmd (self, message):
+    """приветсвую"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/479",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def rofllcmd (self, message):
+    """ШУТКА"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/475",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def zaderjkacmd (self, message):
+    """задержка в развитии"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/474",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def stupidcmd (self, message):
+    """ДЕбил малолетний"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/471",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def welawcmd (self, message):
+    """В ЧЁМ Я ВИНОВАТ?"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/470",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def gdecmd (self, message):
+    """ЧТО КАК КТО ГДЕ"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/469",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def streamcmd (self, message):
+    """стрим"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/468",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def budicmd (self, message):
+    """будильник"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/467",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def geniuscmd (self, message):
+    """это гениально"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/466",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def kringecmd (self, message):
+    """кринж"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/465",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def dotacmd (self, message):
+    """БЫТЬ ДОТЕРОМ"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/464",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def debilcmd (self, message):
+    """какой же ты дебил"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/459",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def papichcmd (self, message):
+    """ПАПИЧ О ДС3"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/458",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def crazycmd (self, message):
+    """crazy"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/457",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def vechercmd (self, message):
+    """неплохой вечер"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/456",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def gimncmd (self, message):
+    """VI KA  Гимн Папича"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/455",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def voncmd (self, message):
+    """ВОН БЛЯТЬ ЕБАШЬ ЕГО"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/454",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def kogocmd (self, message):
+    """КОГО ЭТО ЕБЁТ"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/453",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def mujikcmd (self, message):
+    """mijik"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/450",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def tvareicmd (self, message):
+    """КОНТИНГЕНТ ТВАРЕЙ"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/446",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def aphcicmd (self, message):
+    """АПЧХИ АР Р Р Р Р"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/443",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
+
+ async def jivucmd (self, message):
+    """Я СНОВА ЖИВУ"""
+
+ reply = await message.get_reply_message()
+ await message.delete()
+ await message.client.send_file(
+ message.to_id,
+ "https://t.me/papichvoice/442",
+ voice_note=True,
+ reply_to=reply.id if reply else None,
+ )
+ return
